295d3956dc8de3e7abe4deef451f568e07176df3
Burkhard Mallmes <burkhard.mallmes@generali.com>
2016-06-09 10:41:25 +0200

    Kommentar zur Multiply-Klasse hinzugefuegt
    Conflicts:
    	org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Multiply.java

---
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Multiply.java
@@ -0,0 +1,22 @@
+package org.eclipse.example.calc.internal.operations;
+
+import org.eclipse.example.calc.BinaryOperation;
+
+/**
+ * 
+ * @author V165630
+ *
+ */
+public class Multiply extends AbstractOperation implements BinaryOperation {
+
+	@Override
+	public String getName() {
+		return "/";
+	}
+
+	@Override
+	public float perform(float arg1, float arg2) {
+		return arg1 / arg2;
+	}
+
+}
